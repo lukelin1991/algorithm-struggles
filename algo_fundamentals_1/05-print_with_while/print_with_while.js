3aa2815a4615bb833a424789b958700f7eb80197
github username <github email address>
2020-04-28 19:40:33 -0400

    solutions for some

--- algo_fundamentals_1/05-print_with_while/print_with_while.js
+++ algo_fundamentals_1/05-print_with_while/print_with_while.js
@@ -1,1 +1,8 @@
 // print integers from 1800 to 3400, using a while.
+// interesting...
+let i = 1800
+
+while(i < 3400){
+    console.log(i)
+    i+=1
+}
--- algo_fundamentals_1/05-print_with_while/print_with_while.rb
+++ algo_fundamentals_1/05-print_with_while/print_with_while.rb
@@ -1,1 +1,7 @@
 # print integers from 1800 to 3400, using a while.
+i = 1800
+
+while i <= 3400 do
+    puts i
+    i += 1
+end
