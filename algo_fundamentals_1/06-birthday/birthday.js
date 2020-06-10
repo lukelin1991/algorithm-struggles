8abc9b87cf10eb2248a3e4e51734c9a9e0b5597f
github username <github email address>
2020-06-10 12:45:25 -0400

    test

--- algo_fundamentals_1/06-birthday/birthday.js
+++ algo_fundamentals_1/06-birthday/birthday.js
@@ -1,2 +1,7 @@
 // if 2 given numbers represent your birth month and day in either order, 
 // put "How did you know?" else put "Just another day..."
+
+let num1 = 8
+let num2 = 1
+let num3 = 6
+let num4 = 9
