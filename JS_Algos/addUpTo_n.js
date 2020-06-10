bc0d5dcdd8331760423bf99219983f8e7e340d64
github username <github email address>
2020-06-10 13:30:52 -0400

    speed testing

--- JS_Algos/addUpTo_n.js
+++ JS_Algos/addUpTo_n.js
@@ -14,3 +14,9 @@ function addUpTo(n){
 function addUpTo(n){
     return n * (n + 1) / 2;
 }
+
+// speed test
+var t1 = performance.now();
+addUpTo(1000000000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000 } seconds.`)
